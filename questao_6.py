d94e304c2b4bd44fd9ae701bf4df18f12068812c
MateusCLima11 <mateuslimasesi@gmail.com>
2025-09-06 15:40:48 -0300

    Update questao_6.py

--- questao_6.py
+++ questao_6.py
@@ -1,13 +1,10 @@
 import os
 os.system("cls")
 
-# Entrada das notas
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 
-
 media = (nota1 + nota2) / 2
-
 
 print(f"Média do aluno: {media:.2f}")
 
@@ -18,3 +15,4 @@ elif 4.1 <= media <= 5.9:
     print("Você está na recuperação.")
 else:
     print("Reprovado.")
+
